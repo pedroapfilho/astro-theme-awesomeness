f9728a4fa218c610b321b704bb92c11794badb37
Pedro Filho <pedro@filho.me>
2026-05-20 17:31:38 -0300

    feat(astro-awesomeness/lib): barrel

---
+++ packages/astro-awesomeness/src/lib/index.ts
@@ -0,0 +1,5 @@
+export { cn } from "./cn";
+export { formatDate } from "./format-date";
+export { getRelatedPosts } from "./get-related-posts";
+export { readingTime } from "./reading-time";
+export { slugify } from "./slugify";
