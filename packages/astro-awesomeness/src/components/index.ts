36f16258221927c3f4daa704baed1ac55124e11d
Pedro Filho <pedro@filho.me>
2026-05-20 17:38:51 -0300

    feat(astro-awesomeness): components barrel

---
+++ packages/astro-awesomeness/src/components/index.ts
@@ -0,0 +1,22 @@
+export { Button, buttonVariants } from "./ui/button";
+export {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "./ui/command";
+export {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+export { CommandMenu, type CommandMenuItem, type CommandMenuProps } from "./command-menu";
+export { ThemeToggle } from "./theme-toggle";
