63c3619e6a063133fb1e2dbe0bd0c888eb795f8c
Pedro Filho <pedro@filho.me>
2026-08-19 14:40:50 -0300

    Add Zod compiler integration (#61)

--- apps/demo/astro.config.ts
+++ apps/demo/astro.config.ts
@@ -3,13 +3,14 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import zodCompiler from "zod-compiler/vite";
 
 const config = defineConfig({
   integrations: [react(), mdx(), sitemap()],
   site: "https://demo.astro-awesomeness.dev",
   vite: {
     optimizeDeps: { exclude: ["astro-awesomeness"] },
-    plugins: [tailwindcss()],
+    plugins: [zodCompiler(), tailwindcss()],
   },
 });
 
